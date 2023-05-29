31647ceec0b9e78f8aa807d3f81cbbe35d3d1850
leo <3181538941@qq.com>
2023-05-29 14:37:57 +0800

    实现手机评论数据可视化

--- apps/visual/urls.py
+++ apps/visual/urls.py
@@ -4,9 +4,10 @@
 # @date 2023/5/29
 from django.urls import path
 
-from apps.visual.views import BrandView
+from apps.visual.views import BrandView, Echarts
 
 urlpatterns = [
     # path('', CenterView.as_view()),
     path('brands/visual/', BrandView.as_view()),
+    path('phones/echarts/', Echarts.as_view()),
 ]
--- apps/visual/views.py
+++ apps/visual/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from apps.crawler.models import Phone_brand
+from apps.crawler.models import Phone_brand, Phone_sku
 from django.views import View
 from loguru import logger
 from django.http import JsonResponse
@@ -26,3 +26,21 @@ class BrandView(View):
         cache_brands = cache_handler('brands_pst', get_brands_pst, 30 * 12 * 60 * 60)
         logger.debug(f"{cache_brands=}")
         return JsonResponse(cache_brands, safe=False)
+
+
+def get_phone_cmt():
+    res = {'code': 200, 'data': {'xAxis': [], 'series': []}}
+    # 获取手机中评论数最多的前10个品牌
+    phones = Phone_sku.objects.order_by('-comments_num')[:10]
+
+    for phone in phones:
+        res['data']['xAxis'].append(phone.name)
+        res['data']['series'].append(phone.comments_num)
+    return res
+
+
+class Echarts(View):
+    def get(self, request):
+        phone_cmt = cache_handler('phone_cmt', get_phone_cmt, 30)
+        logger.debug(f"{phone_cmt=}")
+        return JsonResponse(phone_cmt, safe=False)
